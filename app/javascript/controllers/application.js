08c82ed0e6596f11f43c8194b8e5940182759a6d
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-07-26 17:45:13 +0400

    init nav partial and styles

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,17 @@
-import { Application } from "@hotwired/stimulus"
+import { Application, Controller } from "@hotwired/stimulus"
 
 const application = Application.start()
+
+application.register("nav", class extends Controller {
+  static targets = ["menu"]
+
+  connect(){
+  }
+
+  handleOnClick(){
+    this.menuTarget.classList.toggle("hidden");
+  }
+})
 
 // Configure Stimulus development experience
 application.debug = false
